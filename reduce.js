406f280e00e0a056596670b79605803d548b5b43
Shahidul Islam <shahid.okay@gmail.com>
2022-07-03 23:34:48 +0000

    practice js reduce

--- reduce.js
+++ reduce.js
@@ -3,7 +3,10 @@
  * To run this file in Gitpod, use the 
  * command node reduce.js in the terminal
  */
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0)
 
+console.log(sum)
 
 // Summing an array of numbers:
 
@@ -32,6 +35,52 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0)
+console.log(totalExperience)
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if(!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience
+  }
+  return acc
+},{})
+console.log(experienceByProfession)
+
+
+
+const euros = [5, 10, 15];
+
+const sum1 = euros.reduce((total, amount) => total + amount)
+console.log(sum1)
+
+// calcualte average
+
+const average = euros.reduce((total, amount, index, array) => {
+  total += amount;
+  if(index === array.length -1) {
+    return total / array.length;
+  } else {
+    return total
+  }
+})
+console.log(average)
+
+
+const doubled = euros.reduce((total, amount) => {
+  total.push(amount * 2)
+  return total
+}, []);
+console.log(doubled)
+
+const above10 = euros.reduce((total, amount) => {
+  if (amount > 10){
+    total.push(amount)
+  }
+  return total;
+},[])
+console.log(above10)
